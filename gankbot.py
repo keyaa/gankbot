324e7a7715ceffcbb4d15832cd47089899d3355e
root <keya5541@colorado.edu>
2017-12-23 14:34:28 -0800

    Formatting fixes

--- gankbot.py
+++ gankbot.py
@@ -44,21 +44,22 @@ async def on_message(message):
 	if message.content.startswith(">help"):
 		await client.send_message(message.author, "**------ Commands ------**\n\
 Prefix all commands with > character.\n\
-ping: 		Generic response test for robot\n\
-stop: 		Stop currently playing audio and leave voice channel\n\
-play ___: 		Starts playing Youtube link\n\
-oof #: 		Roblox death sound _(must be in voice channel, limit 10)_\n\
-OOF #: 		Loud Roblox death sound _(must be in voice channel, limit 3)_\n\
-thot #:		Begone, thot! _(must be in voice channel, limit 3)_\n\
-THOT #: 		***BEGONE, THOT!*** _(must be in voice channel, limit 1)_\n\
-quack #:		Duck noise _(must be in voice channel, limit 10)_\n\
-woop #: 		Unknown noise _(must be in voice channel, limit 10)_\n\
-weeb #: 		Fetches most recent post on r/awwnime _(limit 3)_\n\
-keeb #: 		Fetches most recent post on r/mechanicalkeyboards _(limit 3)_\n\
-haiku #: 		Fetches most recent post on r/youtubehaiku _(limit 3)_\n\
-me #: 		Fetches most recent post on r/me_irl _(limit 3)_\n\
-code #: 		Fetches most recent post on r/programmerhumor _(limit 3)_\n\
-video #: 		Fetches most recent post on r/videos _(limit 3)_")
+# character is optional, specifies number to repeat.\n\
+ping: 		Generic response test for robot.\n\
+stop: 		Stop currently playing audio and leave voice channel.\n\
+play 'link': 		Starts playing Youtube link.\n\
+oof #: 		Roblox death sound _(must be in voice channel, limit 10)_.\n\
+OOF #: 		Loud Roblox death sound _(must be in voice channel, limit 3)_.\n\
+thot #:		Begone, thot! _(must be in voice channel, limit 3)_.\n\
+THOT #: 		***BEGONE, THOT!*** _(must be in voice channel, limit 1)_.\n\
+quack #:		Duck noise _(must be in voice channel, limit 10)_.\n\
+woop #: 		Unknown noise _(must be in voice channel, limit 10)_.\n\
+weeb #: 		Fetches most recent post on r/awwnime _(limit 3)_.\n\
+keeb #: 		Fetches most recent post on r/mechanicalkeyboards _(limit 3)_.\n\
+haiku #: 		Fetches most recent post on r/youtubehaiku _(limit 3)_.\n\
+me #: 		Fetches most recent post on r/me_irl _(limit 3)_.\n\
+code #: 		Fetches most recent post on r/programmerhumor _(limit 3)_.\n\
+video #: 		Fetches most recent post on r/videos _(limit 3)_.")
 	elif message.content.startswith(">stop"):
 		for voice in client.voice_clients:
 			if voice.server == message.channel.server:
