ad539216adf44b1701b720658000152573db9988
Kevin Yang <keya5541@colorado.edu>
2018-01-17 22:01:24 -0700

    Added Owen Wilson wow

--- gankbot.py
+++ gankbot.py
@@ -56,6 +56,8 @@ oof #: 		Roblox death sound _(must be in voice channel, limit 10)_.\n\
 OOF #: 		Loud Roblox death sound _(must be in voice channel, limit 3)_.\n\
 thot #:		Begone, thot! _(must be in voice channel, limit 3)_.\n\
 THOT #: 		***BEGONE, THOT!*** _(must be in voice channel, limit 1)_.\n\
+wow #:		Owen Wilson 'wow' _(must be in voice channel, limit 10)_.\n\
+WOW #:		Every Owen Wilson 'wow' _(must be in voice channel, limit 3)_.\n\
 quack #:		Duck noise _(must be in voice channel, limit 10)_.\n\
 git #:		git text-to-speech _(must be in voice channel, limit 10)_.\n\
 woop #: 		Unknown noise _(must be in voice channel, limit 10)_.\n\
@@ -151,6 +153,34 @@ video #: 		Fetches most recent post on r/videos _(limit 3)_.")
 			repetitions = 1
 		for i in range(0, int(repetitions)):
 			player = voice.create_ffmpeg_player("assets/loudthot.mp3")
+			player.start()
+			while not player.is_done():
+				await asyncio.sleep(.01)
+		await voice.disconnect()
+	elif message.content.startswith(">wow"):
+		channel = client.get_channel(message.author.voice.voice_channel.id)
+		voice = await client.join_voice_channel(channel)
+		repetitions = message.content[5:]
+		if repetitions == "":
+			repetitions = 1
+		if int(repetitions) > 10:
+			repetitions = 10
+		for i in range(0, int(repetitions)):
+			player = voice.create_ffmpeg_player("assets/quietwow.mp3")
+			player.start()
+			while not player.is_done():
+				await asyncio.sleep(.01)
+		await voice.disconnect()
+	elif message.content.startswith(">WOW"):
+		channel = client.get_channel(message.author.voice.voice_channel.id)
+		voice = await client.join_voice_channel(channel)
+		repetitions = message.content[5:]
+		if repetitions == "":
+			repetitions = 1
+		if int(repetitions) > 3:
+			repetitions = 3
+		for i in range(0, int(repetitions)):
+			player = voice.create_ffmpeg_player("assets/loudwow.mp3")
 			player.start()
 			while not player.is_done():
 				await asyncio.sleep(.01)
